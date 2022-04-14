2ff080ccb2b02477947b7507bcac0e4b8487d2bb
maxhirtens <max@maxhirtenstein.com>
2022-04-13 17:09:58 -0700

    finished exercise

--- find-findindex.js
+++ find-findindex.js
@@ -1,5 +1,5 @@
-/* 
-Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+/*
+Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined.
 
 const users = [
   {username: 'mlewis'},
@@ -11,11 +11,20 @@ findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function(user){
+    return user.username === username;
+  });
+}
 
 /*
-Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined.
 
 const users = [
   {username: 'mlewis'},
@@ -27,4 +36,11 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+
+function removeUser(usersArray, username) {
+  const idx = usersArray.findIndex(function(user){
+   return user.username === username;
+    });
+    if(idx === -1) return undefined;
+    else return usersArray.splice(idx, idx+1)[0];
+  }
